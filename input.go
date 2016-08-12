56db797ac509c33e03eb2dcfcbdf20416ab80c8d
Adam Mckaig <adam.mckaig@gmail.com>
2016-08-11 21:44:18 -0400

    Move input path stuff to GetInputPath

---
+++ input.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"github.com/adammck/venv"
+	"github.com/blang/vfs"
+)
+
+func GetInputPath(fs vfs.Filesystem, env venv.Env) string {
+
+	var fn string
+
+	fn = env.Getenv("TF_STATE")
+	if fn != "" {
+		return fn
+	}
+
+	fn = env.Getenv("TI_TFSTATE")
+	if fn != "" {
+		return fn
+	}
+
+	fn = "terraform.tfstate"
+	_, err := fs.Stat(fn)
+	if err == nil {
+		return fn
+	}
+
+	return ""
+}
