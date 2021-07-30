693e714ab7ac935324dc41d3ae5409df70fa0116
knobik <knobiks@gmail.com>
2021-07-30 23:09:12 +0200

    Add telmate/proxmox provider (#157)

--- resource.go
+++ resource.go
@@ -38,6 +38,8 @@ func init() {
 		"nic_list.0.ip_endpoint_list.0.ip",                    // Nutanix
 		"network_interface.0.nat_ip_address",                  // Yandex
 		"network_interface.0.ip_address",                      // Yandex
+		"default_ipv4_address",                                // Telmate/Proxmox
+		"ssh_host",                                            // Telmate/Proxmox
 	}
 
 	// Formats:
